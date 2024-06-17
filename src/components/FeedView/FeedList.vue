5252e9ab1a1060aeab0b5d277bd2e95a0638cdc5
migugin <xiahtic73@gmail.com>
2024-06-17 16:41:37 +0900

    [feat] 에러 핸들링 추가

--- src/components/FeedView/FeedList.vue
+++ src/components/FeedView/FeedList.vue
@@ -1,15 +1,17 @@
 <template>
-  <div v-if="renderData.length > 0">
-    <ul>
-      <FeedItem
-        v-for="data in renderData"
-        :key="data.id"
-        :postData="data"
-      />
-    </ul>
-  </div>
-  <div v-else>
-    <p class="no-data">데이터가 없습니다.</p>
+  <div>
+    <div v-if="error">
+      <p>데이터를 불러오는 중 문제가 발생했습니다: {{ errorMessage }}</p>
+      <button @click="fetchData">재시도</button>
+    </div>
+    <div v-else>
+      <div v-if="renderData.length > 0">
+        <ul>
+          <FeedItem v-for="data in renderData" :key="data.id" :postData="data" />
+        </ul>
+      </div>
+      <p v-else class="no-data">데이터가 없습니다.</p>
+    </div>
   </div>
 </template>
 
@@ -26,10 +28,32 @@ export default {
     renderData() {
       return this.$store.state.renderData;
     },
+    
+    errorMessage() {
+      if (this.error.response && this.error.response.data.message) {
+        return this.error.response.data.message;
+      }
+      return this.error.message || "알 수 없는 에러가 발생했습니다.";
+    }
   },
 
-  beforeMount() {
-    this.$store.dispatch("getData");
+  methods: {
+    async fetchData() {
+      this.error = "";
+      this.loading = true;
+
+      try {
+        await this.$store.dispatch("getData");
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+
+  mounted() {
+    this.fetchData();
   },
 };
 </script>
